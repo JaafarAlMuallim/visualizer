f1e16c5bedd924310ff727b3de4b1e36f1d85d63
JaafarAlMuallim <Ja3far03@gmail.com>
2023-08-02 23:32:59 +0300

    refactored toasts to get the classes and color according to the type of message to be sent

--- utils/toasts.tsx
+++ utils/toasts.tsx
@@ -1,14 +1,22 @@
 import { toast } from "@/components/ui/use-toast";
 
 const customToast = (message: string, type: string) => {
-  const color =
-    type === "error" ? "red" : type === "warning" ? "orange" : "green";
+  let className = "";
+  if (type === "error") {
+    className = "bg-red-700 text-white border-2 border-red-700";
+  } else if (type === "warning") {
+    className = "bg-orange-700 text-white border-2 border-orange-700";
+  } else if (type === "update") {
+    className = "bg-blue-900 text-white border-2 border-blue-900";
+  } else {
+    className = "bg-green-700 text-white border-2 border-green-700";
+  }
   const title = type.charAt(0).toUpperCase() + type.slice(1);
   toast({
     title: title,
     description: message,
     duration: 2000,
-    className: `bg-${color}-700 text-white border-2 border-${color}-700`,
+    className: className,
   });
 };
 
