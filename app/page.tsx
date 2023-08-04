6eb000b14d53ce5f1f27510d7966d32be0561432
JaafarAlMuallim <Ja3far03@gmail.com>
2023-08-04 23:43:22 +0300

    updated width of screen

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { motion } from "framer-motion";
 import HomePageContent from "./components/HomePage";
 export default function Home() {
   return (
-    <div className=" bg-gradient-to-r from-slate-700 via-slate-800 to-slate-900 h-screen">
+    <div className=" bg-gradient-to-r from-slate-700 via-slate-800 to-slate-900 h-screen w-screen overflow-hidden">
       <HomePageContent />
     </div>
   );
